18fd759d33a9368591b0a3718740b8875ee874b1
MiguelAngelTG7 <michael_angello@hotmail.com>
2024-10-14 15:46:38 -0500

    fechas solucionado

--- src/Entries.jsx
+++ src/Entries.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Button, Col, Row, Container } from 'react-bootstrap';
 
+// Función para formatear fecha y hora en formato legible
+const formatDateTime = (dateTimeString) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+  return new Date(dateTimeString).toLocaleDateString('en-US', options);
+};
+
 const EntryBody = ({ entryData, removeEntry, editEntry }) => {
   return entryData.map((line, index) => (
     <Container key={index} className="shadow p-3 mb-5 bg-white rounded">
       <Row style={{ padding: '10px'}}>
         <Col>
           <h2>{line.title}</h2>
-          <small>Fecha Aqui</small>
-          {/*<mark className="text-danger">{Date()}</mark>*/}
+          <mark>Creado: {formatDateTime(line.created_at)}</mark><br/>
+          <mark>Actualizado: {formatDateTime(line.updated_at)}</mark>
           <p>{line.body}</p>
           <Button variant="primary" className="shadow" onClick={() => editEntry(index)}>Edit</Button>{' '}
           <Button variant="danger" className="shadow" onClick={() => removeEntry(index)}>Delete</Button>
