7be7a9f32da735a4e5dfd7c34994250bfd6f0ead
MiguelAngelTG7 <michael_angello@hotmail.com>
2024-11-06 22:41:34 -0500

    otra vez bien y bueno

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 import Entries from './Entries';
 import Forma from './Forma';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  const [entries, setEntries] = useState([]);  // Para almacenar las entradas del diario
-  const [editingIndex, setEditingIndex] = useState(null); // Índice de la entrada que se está editando
-  const [editingEntry, setEditingEntry] = useState(null); // Entrada que se está editando
+  const [entries, setEntries] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editingEntry, setEditingEntry] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  // Método para agregar una entrada
+  // Función para manejar el inicio de sesión
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (email === 'mikicho.junior@gmail.com' && password === '1347') {
+      setIsAuthenticated(true);
+    } else {
+      alert('Correo o contraseña incorrectos');
+    }
+  };
+
+  // Función para manejar el cierre de sesión
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    setEmail('');       // Limpiar el correo
+    setPassword('');    // Limpiar la contraseña
+  };
+
   const handleSubmit = (entry) => {
     axios
       .post('http://127.0.0.1:5000/posteos', entry)
       .then((response) => {
         console.log('Entrada creada:', response.data.message);
         const newEntry = response.data.content;
-        setEntries([...entries, newEntry]);  // Añadimos la nueva entrada al estado
+        setEntries([...entries, newEntry]);
       })
       .catch((error) => {
         console.error('Error creando la entrada:', error);
       });
   };
 
-  // Método para editar una entrada
   const handleEdit = (index) => {
     const entryToEdit = entries[index];
-    setEditingIndex(index); // Guardamos el índice de la entrada que estamos editando
-    setEditingEntry(entryToEdit); // Guardamos la entrada que estamos editando
+    setEditingIndex(index);
+    setEditingEntry(entryToEdit);
   };
 
-  // Método para actualizar una entrada existente (PUT)
   const handleUpdate = (updatedEntry) => {
     axios
-      .put(`http://127.0.0.1:5000/posteos/${editingEntry.id}`, updatedEntry) // Cambia editingIndex por editingEntry.id
+      .put(`http://127.0.0.1:5000/posteos/${editingEntry.id}`, updatedEntry)
       .then((response) => {
         console.log('Entrada actualizada:', response.data.message);
         const updatedEntries = entries.map((entry) =>
           entry.id === editingEntry.id ? { ...entry, ...updatedEntry } : entry
         );
-        setEntries(updatedEntries);  // Actualizamos el estado con la entrada modificada
-        setEditingIndex(null);  // Limpiamos el índice de edición
-        setEditingEntry(null);  // Limpiamos la entrada de edición
+        setEntries(updatedEntries);
+        setEditingIndex(null);
+        setEditingEntry(null);
       })
       .catch((error) => {
         console.error('Error actualizando la entrada:', error);
       });
   };
 
-  // Método para eliminar una entrada
   const removeEntry = (index) => {
     const entryToRemove = entries[index];
-
     axios
       .delete(`http://127.0.0.1:5000/posteos/${entryToRemove.id}`)
       .then((response) => {
@@ -63,7 +78,6 @@ const App = () => {
       });
   };
 
-  // useEffect para cargar las entradas cuando el componente se monta
   useEffect(() => {
     axios
       .get('http://127.0.0.1:5000/posteos')
@@ -73,40 +87,52 @@ const App = () => {
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-  }, []);  // El array vacío hace que este efecto se ejecute solo una vez cuando el componente se monta
+  }, []);
 
   return (
     <div>
-      <div
-        className="bg-image"
-        style={{
-          backgroundImage: "url('https://img.freepik.com/free-photo/solid-navy-blue-concrete-textured-wall_53876-124584.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1728864000&semt=ais_hybrid-rr-similar')",
-          backgroundRepeat: "repeat", 
-          minHeight: "100vh"
-        }}
-      >
-        <h1 className="display-5 text-white text-center fw-bold text-uppercase" style={{ paddingBottom: '40px', paddingTop: '40px' }}>
-          My Journal App
-        </h1>
-        <div className="container text-center border border-3 rounded" style={{ padding: '50px' }}>
-          <div className="row">
-            <div className="col">
-              <h2 className="text-white fw-bold">Today</h2>
-              {/* Mostramos el formulario para agregar o editar */}
-              {editingIndex === null ? (
-                <Forma handleSubmit={handleSubmit} />
-              ) : (
-                <Forma handleSubmit={handleUpdate} entry={editingEntry} />
-              )}
-            </div>
-            <div className="col-1"></div>
-            <div className="col-7">
-              {/* Mostramos las entradas */}
-              <Entries entryData={entries} removeEntry={removeEntry} editEntry={handleEdit} />
+      {isAuthenticated ? (
+        <div className="bg-image" style={{ backgroundImage: "url('https://img.freepik.com/free-photo/solid-navy-blue-concrete-textured-wall_53876-124584.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1728864000&semt=ais_hybrid-rr-similar')", backgroundRepeat: "repeat", minHeight: "100vh" }}>
+          <h1 className="display-5 text-white text-center fw-bold text-uppercase" style={{ paddingBottom: '40px', paddingTop: '40px' }}>My Journal App</h1>
+          
+          {/* Botón de Cerrar Sesión */}
+          <div className="text-end me-5">
+            <button className="btn btn-danger" onClick={handleLogout}>Salir</button>
+            <br /> <br />
+          </div>
+
+          <div className="container text-center border border-3 rounded" style={{ padding: '50px' }}>
+            <div className="row">
+              <div className="col">
+                <h2 className="text-white fw-bold">Today</h2>
+                {editingIndex === null ? (
+                  <Forma handleSubmit={handleSubmit} />
+                ) : (
+                  <Forma handleSubmit={handleUpdate} entry={editingEntry} />
+                )}
+              </div>
+              <div className="col-1"></div>
+              <div className="col-7">
+                <Entries entryData={entries} removeEntry={removeEntry} editEntry={handleEdit} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="login-container text-center" style={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#f5f5f5" }}>
+          <form onSubmit={handleLogin} style={{ width: "300px", padding: "20px", background: "white", borderRadius: "5px", boxShadow: "0 4px 8px rgba(0,0,0,0.2)" }}>
+            <h2 className="mb-8 text-danger text-center fw-bold text-uppercase">Journal App</h2>
+            <h2 className="mb-4">Iniciar Sesión</h2>
+            <div className="mb-3">
+              <input type="email" className="form-control" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            </div>
+            <div className="mb-3">
+              <input type="password" className="form-control" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            </div>
+            <button type="submit" className="btn btn-primary w-100">Ingresar</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
